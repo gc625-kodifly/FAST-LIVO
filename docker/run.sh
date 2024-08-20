a5eabfe22ce2c9a758ec9d19bc1aa84e7044be9b
gc625-kodifly <gabriel.chan@kodifly.com>
2024-08-20 11:50:46 +0800

    update laucnhc

--- docker/run.sh
+++ docker/run.sh
@@ -2,10 +2,12 @@
 
 xhost +local:docker  # More secure than 'xhost +'
 
-docker run -it --rm --gpus all --net=host --privileged \
+docker run -it --rm --runtime=nvidia --net=host --privileged \
   -e DISPLAY=$DISPLAY \
-  -v /home/gabriel/fast_livo_ws:/home/admin/workspace \
+  -v /home/kodifly/livo_ws/:/home/admin/workspace \
+  -v /home/kodifly/timeshare:/home/admin/timeshare \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $HOME/.Xauthority:/home/admin/.Xauthority:rw \
-  docker.io/library/fast_livo:latest \
+  -v /dev:/dev \
+  fast_livo:arm \
   /bin/bash
