d187878f433e416a5043b8ea5f34ca0d20c3ea29
gc625 <gabrielchan625@gmail.com>
2024-08-26 17:34:44 +0800

    update docker stuff

--- docker/run.sh
+++ docker/run.sh
@@ -4,10 +4,10 @@ xhost +local:docker  # More secure than 'xhost +'
 
 docker run -it --rm --runtime=nvidia --net=host --privileged \
   -e DISPLAY=$DISPLAY \
-  -v /home/kodifly/livo_ws/:/home/admin/workspace \
-  -v /home/kodifly/timeshare:/home/admin/timeshare \
+  -v /home/gabriel/fast_livo_ws/:/home/admin/workspace \
+  -v /home/gabriel/timeshare:/home/admin/timeshare \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $HOME/.Xauthority:/home/admin/.Xauthority:rw \
   -v /dev:/dev \
-  fast_livo:arm \
+  fast_livo:x86 \
   /bin/bash
